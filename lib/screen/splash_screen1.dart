ca569c85a967d817002e4657372c4ad27146e070
Satriahrl_ <satriaherlambang22@gmail.com>
2025-11-07 11:19:56 +0700

    create splash screen scaffold awal

---
+++ lib/screen/splash_screen1.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(SplashScreen1());
+}
+
+class SplashScreen1 extends StatelessWidget {
+  const SplashScreen1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
