7dabbb5e88cdfbce012a7da59eafacf97aa5a7c0
Satriahrl_ <satriaherlambang22@gmail.com>
2025-11-07 14:47:27 +0700

    menambahkan folder dan file login

---
+++ lib/login/login.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Login());
+}
+
+class Login extends StatelessWidget {
+  const Login({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
